d6e8ce72ee1b6d14cc97881d91da64be5df0f690
rlatj <rlatj4769@gmail.com>
2019-07-31 20:02:10 +0900

    commit3

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Blog, Photo 
+from .models import Blog 
              
 
 admin.site.register(Blog)
--- secondproject/wsgi.py
+++ secondproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secondproject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Secondproject")
 
 application = get_wsgi_application()
